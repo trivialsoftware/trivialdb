803633e8cf566145e5a2dd6a246efbd5d2e4efab
Christopher S. Case <chris.case@g33xnexus.com>
2014-12-03 09:32:56 -0600

    updated example.

--- examples/horrible_example.js
+++ examples/horrible_example.js
@@ -20,25 +20,57 @@ function pprint(obj)
 var db = jbase.db('example', { writeToDisk: false });
 
 // Create some new keys
-var hammerID = db.store({ name: "Captain Hammer", role: 'hero', nemeses: [] });
-var horribleID = db.store({ name: "Dr. Horrible", role: 'villian', nemeses: [] });
-var moistID = db.store({ name: "Moist", role: 'henchman', nemeses: [] });
-var pennyID = db.store({ name: "Penny", role: 'love interest', nemeses: []});
+var hammerID, horribleID, moistID, pennyID;
+db.store({ name: "Captain Hammer", role: 'hero', nemeses: [] })
+    .then(function(id)
+    {
+        hammerID = id;
+        return db.store({name: "Dr. Horrible", role: 'villian', nemeses: []});
+    })
+    .then(function(id)
+    {
+        horribleID = id;
+        return db.store({ name: "Moist", role: 'henchman', nemeses: [] });
+    })
+    .then(function(id)
+    {
+        moistID = id;
+        return db.store({ name: "Penny", role: 'love interest', nemeses: []});
+    })
+    .then(function(id)
+    {
+        pennyID = id;
 
-console.log('[Step 1] db.values:\n%s', pprint(db.values));
-
-// Update some values
-db.merge(hammerID, { nemeses: [horribleID], loveInterest: pennyID });
-db.merge(horribleID, { nemeses: [hammerID], loveInterest: pennyID });
-
-console.log('[Step 2] db.values:\n%s', pprint(db.values));
-
-// Find everyone for whom Penny is a love interest
-var lovesPenny = db.filter(function(key, value)
-{
-    return value.loveInterest == pennyID;
-});
-
-console.log('[Step 3] people who love Penny:\n%s', pprint(lovesPenny));
+        // We've finished adding values, so print out the database:
+        console.log('\n[Step 1] db.values:\n%s', pprint(db.values));
+    })
+    .then(function()
+    {
+        // Update some values
+        return db.merge(hammerID, { nemeses: [horribleID], loveInterest: pennyID });
+    })
+    .then(function()
+    {
+        // Update some values
+        return db.merge(horribleID, { nemeses: [hammerID], loveInterest: pennyID });
+    })
+    .then(function()
+    {
+        // We've finished updating values, so print out the database:
+        console.log('\n[Step 2] db.values:\n%s', pprint(db.values));
+    })
+    .then(function()
+    {
+        // Find everyone for whom Penny is a love interest
+        return db.filter(function(key, value)
+        {
+            return value.loveInterest == pennyID;
+        });
+    })
+    .then(function(lovesPenny)
+    {
+        // Finally, print out everyone who loves Penny
+        console.log('\n[Step 3] people who love Penny:\n%s', pprint(lovesPenny));
+    });
 
 //----------------------------------------------------------------------------------------------------------------------
