3ff96b22cc4d88ff254c04dcd6c1c0adc24c5dd9
Christopher S. Case <chris.case@g33xnexus.com>
2014-12-03 14:45:45 -0600

    added model api.
    This change makes the entire model api self-contained inside the models.js file, with the exception of the 'defineModel' function in the root of the library. That's fine; it means that using models are entirely an optional thing, but are just as easy to do as low-level database access. Provided the unit tests for this work out well, this should be ready to go shortly.

--- jbase.js
+++ jbase.js
@@ -7,6 +7,7 @@
 var Promise = require('bluebird');
 
 var JDB = require('./lib/jdb');
+var models = require('./lib/models');
 var errors = require('./lib/errors');
 
 //----------------------------------------------------------------------------------------------------------------------
@@ -17,11 +18,7 @@ module.exports = {
         // The constructor of JDB handles the create or load logic.
         return new JDB(name, options);
     },
-    defineModel: function(dbName, schemaDef)
-    {
-        var db = module.exports.db(dbName);
-        db.schema = schemaDef;
-    },
+    defineModel: models.defineModel,
     JDB: JDB,
     Promise: Promise,
     errors: errors
