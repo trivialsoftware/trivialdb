71a984cc50bcfe798049d4fd7c57f46620db678c
Christopher S. Case <chris.case@g33xnexus.com>
2015-11-30 11:56:13 -0600

    added additional api calls to example.

--- examples/horrible_example.js
+++ examples/horrible_example.js
@@ -21,15 +21,16 @@ function pprint(obj)
 var db = trivialdb.db('example', { writeToDisk: false });
 
 // Create some new keys
-var hammerID, horribleID, moistID, pennyID;
+var hammerID, horribleID, moistID, bhSingerID, pennyID;
 Promise.resolve()
     .then(() =>
     {
         return Promise.join(
             db.save({ name: "Captain Hammer", role: 'hero', nemeses: [] }).then((id) => { hammerID = id; }),
-            db.save({name: "Dr. Horrible", role: 'villain', nemeses: []}).then((id) => { horribleID = id; }),
+            db.save({ name: "Dr. Horrible", role: 'villain', nemeses: [] }).then((id) => { horribleID = id; }),
             db.save({ name: "Moist", role: 'henchman', nemeses: [] }).then((id) => { moistID = id; }),
-            db.save({ name: "Penny", role: 'love interest', nemeses: []}).then((id) => { pennyID = id; })
+            db.save({ name: "Bad Horse Singer", role: 'henchman', nemeses: [] }).then((id) => { bhSingerID = id; }),
+            db.save({ name: "Penny", role: 'love interest', nemeses: [] }).then((id) => { pennyID = id; })
         );
     })
     .then(() =>
@@ -56,18 +57,34 @@ Promise.resolve()
     })
     .then(() =>
     {
+        // Update some values
+        var moist = db.get(moistID);
+        moist.henchesFor = horribleID;
+        return db.save(moist);
+    })
+    .then(() =>
+    {
         // We've finished updating values, so print out the database:
         console.log('\n[Step 2] db.values:\n%s', pprint(db.values));
     })
     .then(() =>
     {
         // Find everyone for whom Penny is a love interest
-        return db.filter({ loveInterest: pennyID });
-    })
-    .then((lovesPenny) =>
-    {
-        // Finally, print out everyone who loves Penny
+        var lovesPenny = db.filter({ loveInterest: pennyID });
+
+        // Print out everyone who loves Penny
         console.log('\n[Step 3] people who love Penny:\n%s', pprint(lovesPenny));
+    })
+    .then(() =>
+    {
+        // Find every henchman who henches for Dr. Horrible
+        var horribleHenches = db.query()
+            .filter({ role: 'henchman' })
+            .filter({ henchesFor: horribleID })
+            .run();
+
+        // Print out everyone who loves Penny
+        console.log('\n[Step 3] Henchmen who hench for Dr. Horrible:\n%s', pprint(horribleHenches));
     });
 
 //----------------------------------------------------------------------------------------------------------------------
