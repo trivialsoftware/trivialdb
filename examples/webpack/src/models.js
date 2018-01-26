794f8f254fe585d586a8fc0f63131dbd088e3236
Christopher S. Case <chris.case@g33xnexus.com>
2018-01-25 22:11:48 -0600

    changed to make example more clear.

--- examples/webpack/src/models.js
+++ examples/webpack/src/models.js
@@ -8,45 +8,46 @@ import tdb from '../../../trivialdb';
 
 //----------------------------------------------------------------------------------------------------------------------
 
+// This could come from another source, like an `import` statement.
+const userDict = {
+    'foo@example.com': {
+        name: "Foo User",
+        age: 23,
+        email: "foo@example.com"
+    },
+    'admin@example.com': {
+        name: "Admin User",
+        age: 47,
+        admin: true,
+        email: "admin@example.com"
+    },
+    'some.user@example.com': {
+        name: "Some User",
+        age: 21,
+        email: "some.usern@example.com"
+    },
+    'admin2@example.com': {
+        name: "Charles Xavier",
+        age: 71,
+        admin: true,
+        email: "admin2@example.com"
+    },
+    'bar@example.com': {
+        name: "Bar Bar",
+        age: 31,
+        email: "bar@example.com"
+    }
+};
+
+//----------------------------------------------------------------------------------------------------------------------
+
 const Users = tdb.db('users', {
     writeToDisk: false,
-    loadFromDisk: true,
     pk: 'email',
-    readFunc: (path) =>
-    {
-        return Promise.resolve({
-            'foo@example.com': {
-                name: "Foo User",
-                age: 23,
-                email: "foo@example.com"
-            },
-            'admin@example.com': {
-                name: "Admin User",
-                age: 47,
-                admin: true,
-                email: "admin@example.com"
-            },
-            'some.user@example.com': {
-                name: "Some User",
-                age: 21,
-                email: "some.usern@example.com"
-            },
-            'admin2@example.com': {
-                name: "Charles Xavier",
-                age: 71,
-                admin: true,
-                email: "admin2@example.com"
-            },
-            'bar@example.com': {
-                name: "Bar Bar",
-                age: 31,
-                email: "bar@example.com"
-            }
-        });
-    }
+    readFunc: () => Promise.resolve(userDict)
 });
 
-const Books = tdb.db('books', {});
+const Books = tdb.db('books', { writeToDisk: false });
 
 //----------------------------------------------------------------------------------------------------------------------
 
